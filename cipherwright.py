9702be73177bd1c91be3262c2b752aca1b76bcc2
Hielyr <7991705+Hielyr@users.noreply.github.com>
2024-01-13 10:42:05 -0600

    Update cipherwright.py
    Adjusted the gnerate_passphrase function to 1. Include 1 and 0 in the possible number choices, 2. add additional pisitions for the number to be in.

--- cipherwright.py
+++ cipherwright.py
@@ -6,19 +6,23 @@ def load_words(file_path):
         return [word.strip() for word in file.readlines()]
 
 def generate_passphrase(words, recent_words, buffer_size):
-    # Select words not in recent words
     choice_words = [word for word in words if word not in recent_words]
 
     first_word = random.choice(choice_words).capitalize()
     second_word = random.choice(choice_words).capitalize()
 
-    # Update recent words
     update_recent_words(recent_words, first_word.lower(), buffer_size)
     update_recent_words(recent_words, second_word.lower(), buffer_size)
 
-    number = random.choice('23456789')
-    if random.choice([True, False]):
+    number = random.choice('0123456789')
+    position = random.randint(1, 4)
+
+    if position == 1:
         first_word = number + first_word
+    elif position == 2:
+        first_word = first_word + number
+    elif position == 3:
+        second_word = number + second_word
     else:
         second_word = second_word + number
 
@@ -34,7 +38,7 @@ def update_recent_words(recent_words, word, buffer_size):
 def main():
     words = load_words('CodexVocabulum.txt')
     num_passphrases = int(input("How many passphrases do you want to generate? "))
-    buffer_size = 10  # Size of the recent words buffer
+    buffer_size = 16
     recent_words = deque()
 
     passphrases = [generate_passphrase(words, recent_words, buffer_size) for _ in range(num_passphrases)]
